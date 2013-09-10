e1a4fabbc161b18d097fd50c8ddb219cf3a9ee30
Bob den Otter <bobdenotter@gmail.com>
2013-09-10 20:54:57 +0200

    delete .gitignore in app/database/

--- package.sh
+++ package.sh
@@ -21,7 +21,7 @@ find bolt/vendor -type d -name ".git" | xargs rm -rf
 find bolt/vendor -type d -name "[tT]ests" | xargs rm -rf
 rm -rf bolt/vendor/psr/log/Psr/Log/Test bolt/vendor/symfony/form/Symfony/Component/Form/Test bolt/vendor/twig/twig/lib/Twig/Test
 rm -rf bolt/vendor/twig/twig/test bolt/vendor/swiftmailer/swiftmailer/test-suite
-rm -rf bolt/composer.* bolt/vendor/symfony/locale/Symfony/Component/Locale/Resources/data bolt/.gitignore
+rm -rf bolt/composer.* bolt/vendor/symfony/locale/Symfony/Component/Locale/Resources/data bolt/.gitignore bolt/app/database/.gitignore
 rm -rf bolt/app/view/img/debug-nipple-src.png bolt/app/view/img/*.pxm
 rm -rf bolt/vendor/swiftmailer/swiftmailer/doc bolt/vendor/swiftmailer/swiftmailer/notes
 rm -rf bolt/theme/default bolt/theme/base-2013/to_be_deleted
