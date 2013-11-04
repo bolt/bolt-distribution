73a01cf9a5b1e64bad55c342c0973f26f97b4bc3
Bob den Otter <bobdenotter@gmail.com>
2013-11-04 21:12:05 +0100

    Preparing for the 1.3 release

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-FILENAME="bolt_1.2_rc"
+FILENAME="bolt_1.3_beta"
 export COPYFILE_DISABLE=true
 
 cd bolt-git/
@@ -17,15 +17,18 @@ cp -rf bolt-git bolt
 
 rm -rf files/*
 
-find bolt/vendor -type d -name ".git" | xargs rm -rf
-find bolt/vendor -type d -name "[tT]ests" | xargs rm -rf
+#until Krumo is caught up...
+cp -rf ../githubs/krumo bolt/vendor/oodle/
+
+find bolt -type d -name ".git" | xargs rm -rf
+find bolt -type d -name "[tT]ests" | xargs rm -rf
 rm -rf bolt/vendor/psr/log/Psr/Log/Test bolt/vendor/symfony/form/Symfony/Component/Form/Test bolt/vendor/twig/twig/lib/Twig/Test
 rm -rf bolt/vendor/twig/twig/test bolt/vendor/swiftmailer/swiftmailer/test-suite
 rm -rf bolt/composer.* bolt/vendor/symfony/locale/Symfony/Component/Locale/Resources/data bolt/.gitignore bolt/app/database/.gitignore
 rm -rf bolt/app/view/img/debug-nipple-src.png bolt/app/view/img/*.pxm
 rm -rf bolt/vendor/swiftmailer/swiftmailer/doc bolt/vendor/swiftmailer/swiftmailer/notes
 rm -rf bolt/theme/default bolt/theme/base-2013/to_be_deleted
-rm -rf bolt/.scrutinizer.yml bolt/.travis.yml
+rm -rf bolt/.scrutinizer.yml bolt/.travis.yml bolt/codeception.yml bolt/run-functional-tests
 
 # remove ._ files..
 dot_clean .
@@ -45,14 +48,15 @@ chmod -R 777 bolt/files bolt/app/cache bolt/app/config bolt/app/database bolt/th
 # until DBAL is fixed (see: https://github.com/doctrine/dbal/pull/226 )
 # cp overrides/SqliteSchemaManager.php bolt/vendor/doctrine/dbal/lib/Doctrine/DBAL/Schema/SqliteSchemaManager.php
 
+
 # Make the archives..
 cd bolt
 tar -czf ../$FILENAME.tgz * .htaccess
 zip -rq ../$FILENAME.zip * .htaccess
 cd ..
-#cp $FILENAME.tgz ./files/bolt_latest.tgz
+cp $FILENAME.tgz ./files/bolt_latest.tgz
 mv $FILENAME.tgz ./files/
-#cp $FILENAME.zip ./files/bolt_latest.zip
+cp $FILENAME.zip ./files/bolt_latest.zip
 mv $FILENAME.zip ./files/
 
 echo "\nAll done!\n"
