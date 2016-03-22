86fc9e738e538a224ac051347fab6bf99749ecc1
Gawain Lynch <gawain.lynch@gmail.com>
2016-03-22 10:04:37 +0100

    Remove old files

--- firstrun.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-git clone git://github.com/bolt/bolt.git bolt-git
-cd bolt-git/
-curl -s http://getcomposer.org/installer | php
-php composer.phar install --no-dev
-cd ..
-
-echo "\nCloned Bolt, downloaded components!\n"
